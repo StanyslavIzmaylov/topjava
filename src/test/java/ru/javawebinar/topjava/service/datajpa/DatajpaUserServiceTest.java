e2d30ee945d28a1222cc90bb649b337a088ee9c2
StanyslavIzmaylov <monsterfish2@yandex.ru>
2024-06-17 14:25:04 +0300

    HW5

--- src/test/java/ru/javawebinar/topjava/service/datajpa/DatajpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DatajpaUserServiceTest.java
@@ -4,14 +4,9 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.Profiles;
-import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.UserService;
 import ru.javawebinar.topjava.service.UserServiceTest;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 import static ru.javawebinar.topjava.MealTestData.MEAL_MATCHER;
 import static ru.javawebinar.topjava.MealTestData.meals;
@@ -25,8 +20,6 @@ public class DatajpaUserServiceTest extends UserServiceTest {
     @Test
     public void getUserAndMeals() {
         User user = service.getUserMeals(USER_ID);
-        List<Meal> mealList = new ArrayList<>(meals);
-        Collections.reverse(mealList);
-        MEAL_MATCHER.assertMatch(user.getMeals(), mealList);
+        MEAL_MATCHER.assertMatch(user.getMeals(), meals);
     }
 }
